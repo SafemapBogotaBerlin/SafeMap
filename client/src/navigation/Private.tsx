d16f9a4d94fead5c01b2e7a9b3aa4f4ee1b96d94
Grzegorz Antkiewicz <grzegorzantkiewicz@MacBook-Pro-5.local>
2023-10-09 18:10:51 +0200

    feat: mapview and markers and buttons and redux refactor

--- client/src/navigation/Private.tsx
+++ client/src/navigation/Private.tsx
@@ -1,13 +1,19 @@
-import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Home from "../screens/Home";
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from '../screens/Home/Home';
 
 const Stack = createNativeStackNavigator();
 
 export default function Private() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen
+        name='Home'
+        component={Home}
+        options={{
+          headerShown: false,
+        }}
+      />
     </Stack.Navigator>
   );
 }
