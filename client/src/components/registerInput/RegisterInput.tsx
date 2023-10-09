61cc69ebdbec2663852d8d29bb36562e0b932d22
Camilo <camilomafioly@gmail.com>
2023-10-09 06:11:43 -0500

    Feat: Login component with firebase authentication

---
+++ client/src/components/registerInput/RegisterInput.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { View, Text, TextInput } from "react-native";
+import { styles } from "./style";
+
+type RegisterInputProps = {
+  value: string;
+  onChangeText: (text: string) => void;
+  placeholder?: string;
+  valid: boolean;
+  errorMessage: string;
+  secret?: boolean;
+};
+
+const RegisterInput: React.FC<RegisterInputProps> = ({
+  value,
+  onChangeText,
+  placeholder,
+  valid,
+  errorMessage,
+  secret = false,
+}) => {
+  const showError = !valid && value !== "";
+  return (
+    <View style={styles.inputContainer}>
+      {showError && <Text style={styles.errorText}>{errorMessage}</Text>}
+      <TextInput
+        style={[styles.input, showError ? styles.invalid : {}]}
+        onChangeText={onChangeText}
+        placeholder={placeholder}
+        secureTextEntry={secret}
+        value={value}
+      />
+    </View>
+  );
+};
+
+export default RegisterInput;
