f4b8bb10bf54cb0550b506c950698aa21b8345f9
Grzegorz Antkiewicz <grzegorzantkiewicz@MacBook-Pro-5.local>
2023-10-11 16:27:57 +0200

    capital letters

--- client/src/navigation/Public.tsx
+++ client/src/navigation/Public.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Login from "../screens/login/Login";
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from '../screens/Login/Login';
 import Register from '../screens/register/Register';
 
 const Stack = createNativeStackNavigator();
@@ -8,8 +8,8 @@ const Stack = createNativeStackNavigator();
 export default function Public() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="Register" component={Register}/>
+      <Stack.Screen name='Login' component={Login} />
+      <Stack.Screen name='Register' component={Register} />
     </Stack.Navigator>
   );
 }
