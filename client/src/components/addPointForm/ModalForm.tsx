c108efa65173190e3004486703d3a40ca75a72ec
Mikhail Rashkovskiy <miklyx@gmail.com>
2023-10-13 09:44:48 +0200

    fix: minor

--- client/src/components/addPointForm/ModalForm.tsx
+++ client/src/components/addPointForm/ModalForm.tsx
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import { View, TouchableOpacity, Text } from 'react-native';
-=======
 import React, { useRef, useState } from 'react';
 import { Modal, View, TouchableOpacity, Text, Animated } from 'react-native';
->>>>>>> origin/feat/marker-info-recenter-and-danger-icons
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import SelectDropdown from 'react-native-select-dropdown';
 import { styles } from './style';
@@ -24,7 +19,6 @@ const ModalForm = () => {
     (state: RootState) => state.home.selectedPoint
   );
 
-<<<<<<< HEAD
   const eventType = ['Robbery', 'Massshooting', 'Police', 'Scary Police'];
   const times = [
     'Now',
@@ -52,23 +46,6 @@ const ModalForm = () => {
     }
   };
   /* const hideForm = () => {
-=======
-const ModalForm: React.FC<ModalFormProps> = ({
-  isVisible,
-  onClose,
-}: ModalFormProps) => {
-  const [selectedEventType, setSelectedEventType] = useState<string | null>(
-    null
-  );
-  const slideAnimation = useRef(new Animated.Value(500)).current;
-  const selectedPoint: Coordinates = useSelector(
-    (state: RootState) => state.home.selectedPoint
-  );
-
-  const eventType = ['Robbery', 'Masshooting', 'Police'];
-
-  const hideForm = () => {
->>>>>>> origin/feat/marker-info-recenter-and-danger-icons
     Animated.timing(slideAnimation, {
       toValue: 500,
       duration: 300,
@@ -92,7 +69,6 @@ const ModalForm: React.FC<ModalFormProps> = ({
   };
 
   return (
-<<<<<<< HEAD
     <View style={styles.container}>
       <SelectDropdown
         data={eventType}
@@ -164,46 +140,6 @@ const ModalForm: React.FC<ModalFormProps> = ({
           return <FontAwesome name={'search'} color={'#444'} size={18} />;
         }}
       />
-=======
-    <Modal visible={isVisible} transparent animationType='slide'>
-      <Animated.View style={{ transform: [{ translateY: slideAnimation }] }}>
-        <View style={styles.container}>
-          <SelectDropdown
-            data={eventType}
-            onSelect={(selectedItem) => {
-              setSelectedEventType(selectedItem);
-            }}
-            defaultButtonText={'Select event type'}
-            buttonTextAfterSelection={(selectedItem) => {
-              return selectedItem;
-            }}
-            rowTextForSelection={(item) => {
-              return item;
-            }}
-            buttonStyle={styles.dropdownBtnStyle}
-            buttonTextStyle={styles.dropdownBtnTxtStyle}
-            renderDropdownIcon={(isOpened) => {
-              return (
-                <FontAwesome
-                  name={isOpened ? 'chevron-up' : 'chevron-down'}
-                  color={'#444'}
-                  size={18}
-                />
-              );
-            }}
-            dropdownIconPosition={'right'}
-            dropdownStyle={styles.dropdownStyle}
-            rowStyle={styles.dropdownRowStyle}
-            rowTextStyle={styles.dropdownRowTxtStyle}
-            selectedRowStyle={styles.selectedRowStyle}
-            searchInputStyle={styles.searchInputStyle}
-            searchPlaceHolder={'Search here'}
-            searchPlaceHolderColor={'darkgrey'}
-            renderSearchInputLeftIcon={() => {
-              return <FontAwesome name={'search'} color={'#444'} size={18} />;
-            }}
-          />
->>>>>>> origin/feat/marker-info-recenter-and-danger-icons
 
       <TouchableOpacity onPress={handleFormSubmit} style={styles.button}>
         <Text style={styles.buttonText}>Submit</Text>
