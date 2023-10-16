69c824092d5bbaace5daf8734897de7a85e025cc
Mikhail Rashkovskiy <miklyx@gmail.com>
2023-10-16 15:01:37 +0200

    feat: added spinner component

---
+++ client/src/components/spinner/Spinner.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {ActivityIndicator, StyleSheet, View} from 'react-native';
+import { styles } from './style';
+
+const Spinner = () => (
+  <View style={[styles.container, styles.horizontal]}>
+    <ActivityIndicator size="large" color="##0C5A1E" />
+  </View>
+);
+
+export default Spinner;
