6d8de949107c5a6e948fd53c638e782c7ecb1d55
Camilo <camilomafioly@gmail.com>
2023-10-17 09:59:22 -0500

    refactor: removed comments and added code indentation

--- client/__tests__/geolocationHelper.test.ts
+++ client/__tests__/geolocationHelper.test.ts
@@ -1,31 +1,25 @@
-import { geolocationHelper } from '../src/helpers/geolocation';
+import { geolocationHelper } from "../src/helpers/geolocation";
 
-describe('Tests for geolocationHelper', () => {
-
-  // Testing toRadians function
+describe("Tests for geolocationHelper", () => {
   it("should convert degrees to radians", () => {
     expect(geolocationHelper.toRadians(180)).toEqual(Math.PI);
     expect(geolocationHelper.toRadians(90)).toEqual(Math.PI / 2);
     expect(geolocationHelper.toRadians(0)).toEqual(0);
   });
-
-  // Testing getDistance function
   it("should calculate distance between two coordinates", () => {
     const coord1 = { latitude: 0, longitude: 0 };
     const coord2 = { latitude: 0, longitude: 1 };
 
-    // You might need a known distance between these coordinates or compare against a known formula
     expect(geolocationHelper.getDistance(coord1, coord2)).toBeCloseTo(111194.92664455873);
   });
 
-  // Testing isMarkerVisible function
   it("should check if a marker is visible within a region", () => {
     const marker = { latitude: 5, longitude: 5 };
     const region = {
       latitude: 5,
       longitude: 5,
       latitudeDelta: 2,
-      longitudeDelta: 2
+      longitudeDelta: 2,
     };
 
     expect(geolocationHelper.isMarkerVisible(marker, region)).toBeTruthy();
@@ -39,23 +33,21 @@ describe('Tests for geolocationHelper', () => {
       a: {
         added_dttm: Date.now(),
         coordinates: { latitude: 10, longitude: 10 },
-        danger_type: 'type1',
-        user_id: 'user1'
+        danger_type: "type1",
+        user_id: "user1",
       },
       b: {
         added_dttm: Date.now(),
         coordinates: { latitude: 10.001, longitude: 10.001 },
-        danger_type: 'type2',
-        user_id: 'user2'
-      }
+        danger_type: "type2",
+        user_id: "user2",
+      },
     };
 
     const userLocation = { latitude: 10, longitude: 10 };
 
     const result = geolocationHelper.findPointsWithDanger(points, userLocation);
-    expect(Object.keys(result)).not.toContain('b');
-    expect(Object.keys(result)).toContain('a');
+    expect(Object.keys(result)).not.toContain("b");
+    expect(Object.keys(result)).toContain("a");
   });
-
-
 });
