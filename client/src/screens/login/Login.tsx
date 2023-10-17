91e3f94d46659cc6e70d3f07d9f08a51cc785f63
Grzegorz Antkiewicz <grzegorzantkiewicz@MacBook-Pro-5.local>
2023-10-17 19:13:03 +0200

    improve img quality

--- client/src/screens/login/Login.tsx
+++ client/src/screens/login/Login.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   Text,
   SafeAreaView,
@@ -8,57 +8,62 @@ import {
   TouchableWithoutFeedback,
   Image,
   View,
-  KeyboardAvoidingView, ScrollView, Platform
-} from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import { routes } from "../../routes/public";
-import useLogin from "../../hooks/UseLogin";
-import { styles } from "./style";
+  KeyboardAvoidingView,
+  ScrollView,
+  Platform,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { routes } from '../../routes/public';
+import useLogin from '../../hooks/UseLogin';
+import { styles } from './style';
 import { NavigationProp } from '../../types/index';
-
 
 export default function Login() {
   const { setEmail, setPassword, login } = useLogin();
 
-  const navigation:NavigationProp  = useNavigation();
+  const navigation: NavigationProp = useNavigation();
 
   return (
     <ScrollView
       contentContainerStyle={{ flexGrow: 1 }}
-      keyboardShouldPersistTaps="handled" 
+      keyboardShouldPersistTaps='handled'
     >
-      
-    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.header}></View>
-        <View style={styles.logo} >
-          <Image source={require('../../../assets/safemapLogo.png')} />
-        </View>
-        <View style={styles.title}>
-          <Text style={styles.titleText}>Mapsafe.</Text>
-        </View>
-        <View style={styles.loginContainer}>
-          <TextInput style={styles.input} placeholder="Email" onChangeText={setEmail} />
-          <TextInput
-          style={styles.input}
-          placeholder="Password"
-          secureTextEntry
-          onChangeText={setPassword}
-          />
-        </View>
-        <View style={styles.buttonsContainer}>
-          <TouchableOpacity onPress={login} style={styles.button}>
-            <Text style={styles.buttonText}>Login</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigation.navigate(routes.register)} style={styles.button}>
-            <Text style={styles.buttonText}>Register</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.footer}></View>
-      </SafeAreaView>
-    </TouchableWithoutFeedback>
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <SafeAreaView style={styles.container}>
+          <View style={styles.header}></View>
+          <View style={styles.logo}>
+            <Image source={require('../../../assets/logoSafeMap.png')} />
+          </View>
+          <View style={styles.title}>
+            <Text style={styles.titleText}>Mapsafe.</Text>
+          </View>
+          <View style={styles.loginContainer}>
+            <TextInput
+              style={styles.input}
+              placeholder='Email'
+              onChangeText={setEmail}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder='Password'
+              secureTextEntry
+              onChangeText={setPassword}
+            />
+          </View>
+          <View style={styles.buttonsContainer}>
+            <TouchableOpacity onPress={login} style={styles.button}>
+              <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => navigation.navigate(routes.register)}
+              style={styles.button}
+            >
+              <Text style={styles.buttonText}>Register</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.footer}></View>
+        </SafeAreaView>
+      </TouchableWithoutFeedback>
     </ScrollView>
   );
 }
-
-
