f1371e1cb651b8ba5ae7fe7afb43b85b54625c4c
Grzegorz Antkiewicz <grzegorzantkiewicz@MacBook-Pro-5.local>
2023-10-17 12:09:03 +0200

    adding redux tests

--- client/src/redux/Home/index.ts
+++ client/src/redux/Home/index.ts
@@ -3,11 +3,8 @@ import type { PayloadAction } from '@reduxjs/toolkit';
 import { Coordinates, DataObject } from '../../types';
 
 interface HomeState {
-  hotpoints: Coordinates[];
   data: DataObject;
   selectedPoint: Coordinates | null;
-  status: 'idle' | 'pending' | 'succeeded' | 'rejected';
-  error: string | null;
   isFormOpen: boolean;
   isPointClicked: boolean;
   whatShouldBeOpened: string;
@@ -16,19 +13,8 @@ interface HomeState {
 const data: DataObject = {};
 
 const initialState: HomeState = {
-  // this should be replaced by the call to the real time database
-  hotpoints: [
-    { latitude: 19.532608, longitude: -99.53209 },
-    { latitude: 19.632608, longitude: -99.73209 },
-    { latitude: 19.732608, longitude: -99.03 },
-    { latitude: 4.820954117734985, longitude: -74.06501975735756 },
-  ],
-
   data: data,
-
   selectedPoint: null,
-  status: 'idle',
-  error: null,
   isFormOpen: false,
   isPointClicked: false,
   whatShouldBeOpened: '',
@@ -38,10 +24,6 @@ const homeSlice = createSlice({
   name: 'home',
   initialState: initialState,
   reducers: {
-    // updatePoints: (state) => {
-    //   update a point might be needed later
-    // },
-
     selectPoint: (state, action: PayloadAction<Coordinates>) => {
       state.selectedPoint = { ...action.payload };
     },
